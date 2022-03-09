9f66d6c8474370f9261e1602d1d6846871a510c9
Asaedry <marshallchrismiller@gmail.com>
2022-03-09 10:10:26 -0800

    organize files into folders

--- black.js
+++
@@ -1,9 +0,0 @@
-const black = () => {
-    document.body.innerHTML = '<h1>Goodbye</h1>';
-    document.body.style.color = 'red';
-    document.body.style.textAlign = 'center';
-    document.body.style.backgroundColor = 'black';
-};
-
-const im = document.getElementById('him');
-im.addEventListener('click', black);
